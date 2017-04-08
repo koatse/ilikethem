cc8762a32a8c90e6a1553e0b689857d99762fe3e
Stephen Tse <koatse@gmail.com>
2017-04-08 15:55:44 -0400

    polish recommend app

--- recommend/admin.py
+++ recommend/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import RecommendedContact, Recommendation
-admin.site.register(RecommendedContact)
+#from .models import RecommendedContact, Recommendation
+#admin.site.register(RecommendedContact)
+
+from .models import Recommendation
 admin.site.register(Recommendation)
