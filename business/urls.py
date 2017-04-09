903615894db7c8b3d9086fae2391fea5057a5497
Stephen Tse <koatse@gmail.com>
2017-04-09 09:56:00 -0400

    Added BusinessService/Profile List/Detail generic views

--- business/urls.py
+++ business/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
-from .views import BusinessServiceListView
+from .views import BusinessServiceListView, BusinessServiceDetailView, BusinessProfileListView, BusinessProfileDetailView
 
 urlpatterns = [
-    url(r'^$', BusinessServiceListView.as_view()),
+    url(r'^service/$', BusinessServiceListView.as_view()),
+    url(r'^service/detail/(?P<pk>\d+)/', BusinessServiceDetailView.as_view()),
+    url(r'^profile/$', BusinessProfileListView.as_view()),
+    url(r'^profile/detail/(?P<pk>\d+)/', BusinessProfileDetailView.as_view()),
 ]
--- business/views.py
+++ business/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import BusinessService
+from django.views.generic import DetailView
+from .models import BusinessService, BusinessProfile
 
 class BusinessServiceListView(ListView):
     model = BusinessService
+
+class BusinessServiceDetailView(DetailView):
+    model = BusinessService
+
+class BusinessProfileListView(ListView):
+    model = BusinessProfile
+
+class BusinessProfileDetailView(DetailView):
+    model = BusinessProfile
