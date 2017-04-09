0b4ef6c73a7f88007ed6c526deb05f7d64d5ca35
Stephen Tse <koatse@gmail.com>
2017-04-09 10:39:27 -0400

    Added rein/base.html and footer links

--- business/urls.py
+++ business/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from .views import BusinessServiceListView, BusinessServiceDetailView, BusinessProfileListView, BusinessProfileDetailView
 
 urlpatterns = [
-    url(r'^service/$', BusinessServiceListView.as_view()),
-    url(r'^service/detail/(?P<pk>\d+)/', BusinessServiceDetailView.as_view()),
-    url(r'^profile/$', BusinessProfileListView.as_view()),
-    url(r'^profile/detail/(?P<pk>\d+)/', BusinessProfileDetailView.as_view()),
+    url(r'^service/$', BusinessServiceListView.as_view(), name="service"),
+    url(r'^service/detail/(?P<pk>\d+)/', BusinessServiceDetailView.as_view(), name="service_detail"),
+    url(r'^profile/$', BusinessProfileListView.as_view(), name="profile"),
+    url(r'^profile/detail/(?P<pk>\d+)/', BusinessProfileDetailView.as_view(), name="profile_detail"),
 ]
