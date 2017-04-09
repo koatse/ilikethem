f045d5a4b8a659f5c13a6c10b4c924af8b659a6c
Stephen Tse <koatse@gmail.com>
2017-04-09 13:05:39 -0400

    Added linked view for Business and Recommendations

--- core/views.py
+++ core/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
 from .models import UserProfile
+from business.models import BusinessProfile
+from recommend.models import Recommendation
 
 class UserProfileListView(ListView):
     model = UserProfile
 
 class UserProfileDetailView(DetailView):
     model = UserProfile
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileDetailView, self).get_context_data(**kwargs)
+        context["recommendations"] = Recommendation.objects.filter(ownby=context["object"].pk)
+        context["business_profiles"] = BusinessProfile.objects.filter(ownby=context["object"].pk)
+        return context
