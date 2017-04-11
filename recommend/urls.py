f0d43e5bc96c236503781d63722230a710c7e2d2
Stephen Tse <koatse@gmail.com>
2017-04-11 17:24:05 -0400

    Added MyRecommendation

--- recommend/urls.py
+++ recommend/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
-from .views import RecommendationListView, RecommendationDetailView
+from .views import RecommendationListView, MyRecommendationListView, RecommendationDetailView
 from .views import CreateRecommendation
 
 urlpatterns = [
-    url(r'^$', RecommendationListView.as_view(), name="all"),
+    url(r'^all/$', RecommendationListView.as_view(), name="all"),
+    url(r'^my/$', MyRecommendationListView.as_view(), name="my"),
     url(r'^detail/(?P<pk>\d+)/', RecommendationDetailView.as_view(), name="detail"),
     url(r'^create/$', CreateRecommendation, name="create"),
 ]
--- recommend/views.py
+++ recommend/views.py
@@ -2,17 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
+from core.models import UserProfile
 from .models import Recommendation
 from .forms import RecommendationForm
 
 class RecommendationListView(ListView):
     model = Recommendation
 
+class MyRecommendationListView(ListView):
+    model = Recommendation
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Recommendation.objects.filter(user=self.request.user)
+        else:
+            return Recommendation.objects.all()
+
 class RecommendationDetailView(DetailView):
     model = Recommendation
     
 @login_required
 def CreateRecommendation(request):
+    try:
+        userprofile = UserProfile.objects.get(user=request.user)
+    except:
+        return redirect("core:create_userprofile")
+
     form_class = RecommendationForm
 
     if request.method == "POST":
@@ -23,7 +38,7 @@ def CreateRecommendation(request):
             recommendation.ownby = userprofile
             recommendation.save()
             form.save_m2m()
-            return redirect("recommend:all")
+            return redirect("recommend:my")
     else:
         form = form_class()
 
