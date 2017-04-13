ee869c56e0210f08310ac88373e67f9c908fbbc0
Stephen Tse <koatse@gmail.com>
2017-04-12 20:33:12 -0400

    added generic CreateView for RenovationExperience

--- experience/urls.py
+++ experience/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from .views import ExperienceView
+from .views import CreateRenovationExperienceView
 
 urlpatterns = [
     url(r'^$', ExperienceView.as_view(), name="all"),
+    url(r'^create_reno/$', CreateRenovationExperienceView.as_view(), name="create_reno"),
 ]
--- experience/views.py
+++ experience/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views.generic.list import ListView
 from django.views.generic import TemplateView
+from django.views.generic import CreateView
 from .models import RenovationExperience, TenantExperience, FinancingExperience, TaxExperience
 
 class ExperienceView(TemplateView):
@@ -13,3 +14,16 @@ class ExperienceView(TemplateView):
         context["financing"] = FinancingExperience.objects.all()
         context["tax"] = TaxExperience.objects.all()
         return context
+
+class CreateRenovationExperienceView(CreateView):
+    model = RenovationExperience
+    fields = '__all__'
+    template_name = "experience/create_reno.html"
+
+    def get_success_url(self):
+        return reverse("experience:all")
+
+    #optional extra context to pass to template
+    #def get_context_data(self, **kwargs):
+    #    context = super(CreateRenovationExperienceView, self).get_context_data(**kwargs)
+    #    return context
